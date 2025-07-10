1bd812a6b13ac4a92f3571df5003dcd995be67ed
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-07-10 15:39:33 +0100

    Enables Modes

--- src/stories/ArticleCardDemo.stories.ts
+++ src/stories/ArticleCardDemo.stories.ts
@@ -1,11 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ArticleCardDemo } from "./ArticleCardDemo";
+import { allModes } from "../../.storybook/modes";
 
 const meta = {
   title: "Components/ArticleCard",
   component: ArticleCardDemo,
   parameters: {
     layout: "fullscreen",
+    chromatic: {
+      //🔶 Test each story for ArticleCard in two modes
+      modes: {
+        light: allModes["light"],
+        dark: allModes["dark"],
+      },
+    },
   },
 } satisfies Meta<typeof ArticleCardDemo>;
 
